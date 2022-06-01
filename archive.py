686635dbf760ce8e9188b98601df5d58b12be069
varunvjr <varun.vjr@gmail.com>
2022-06-01 16:56:01 -0700

    Initial Push

---
+++ archive.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#
+# Copyright 2020 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# =============================================================================
+#
+# Consume messages from Confluent Cloud
+# Using Confluent Python Client for Apache Kafka
+#
+# =============================================================================
+
+from confluent_kafka import Consumer
+import json
+import ccloud_lib
+from google.cloud import storage
+
+BUCKET=""
+def getBucket():
+    storage_client=storage.client()
+    buckets = storage_client.list_buckets()
+    for bucket in buckets:
+        BUCKET=bucket.name
+        break
+    return BUCKET
+
+def uploadRecordList(bucket_name,contents,destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(contents)
+
+    print(
+        f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
+    )
+
+if __name__ == '__main__':
+
+    # Read arguments and configurations and initialize
+    args = ccloud_lib.parse_args()
+    config_file = args.config_file
+    topic = args.topic
+    conf = ccloud_lib.read_ccloud_config(config_file)
+
+    # Create Consumer instance
+    # 'auto.offset.reset=earliest' to start reading from the beginning of the
+    #   topic if no committed offsets exist
+    consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    consumer_conf['group.id'] = 'python_example_group_2'
+    consumer_conf['auto.offset.reset'] = 'earliest'
+    consumer = Consumer(consumer_conf)
+    recordList=[]
+    # Subscribe to topic
+    consumer.subscribe([topic])
+
+    # Process messages
+    total_count = 0
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                bucketName=getBucket()
+                uploadRecordList(bucketName,recordList,"maintenance-data01")
+                # No message available within timeout.
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting for message or event/error in poll()")
+                continue
+            elif msg.error():
+                print('error: {}'.format(msg.error()))
+            else:
+                # Check for Kafka message
+                record_key = msg.key()
+                record_value = msg.value()
+		        uploadRecordList("maintenance-data01",record_value,"maintenance-data01")
+                recordList.append(record_value)
+                data = json.loads(record_value)
+                total_count += 1
+                print("Consumed record with key {} and value {}, \
+                      and updated total count to {}"
+                      .format(record_key, record_value, total_count))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
